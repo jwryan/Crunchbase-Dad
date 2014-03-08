467c835e34e66332fdc91a108a1f9576eb0e4c88
James Ryan <jwryan@gmail.com>
2014-03-08 18:42:02 +0000

    Added write to CSV, Json. Read/write to pickle

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from cbaseAPIRequests import getAllInfo
 import json
+import csv
+import pickle
 
-#query = 'testing' #raw_input('Enter search: ')
-
+##Put search query in to getAllInfo argument
+##Comment out if loading pre-saved results
 result = getAllInfo('LED Lighting').results
-with open('results.json', 'w') as outfile:
+
+##Save search results in a python specific format (pickle) for converting to CSV later.
+with open('results.pk', 'wb') as outfile:
+    pickle.dump(result, outfile)
+
+##Save results in JSON as well (had some issues reading the JSON back in correctly thus
+##only supporting loading in pickle files for now
+with open('results.json', 'wb') as outfile:
     json.dump(result, outfile)
+    
+##Open a previously saved pickle file to parse
+#with open('results.pk', 'rb') as infile:
+#    result = pickle.loads(infile)
+#result = pickle.load(open('results.pk','rb'))
+
+#Write
+filename = ''
+properties = ['name', 'category_code', 'description', 'crunchbase_url', 'homepage_url', 'overview']
+with open(r'D:\Test Ing\test.csv', 'wb') as outfile:
+    headers = properties
+    writer = csv.writer(outfile)
+    writer.writerow(headers)
+    companies = result['company']
+    for company in companies.iterkeys():
+        row = []
+        for header in headers:
+            if companies[company][header]:
+                row.append(str(companies[company][header].encode('UTF-8')))
+            else:
+                row.append(''.encode('UTF-8'))
+        writer.writerow(row)
